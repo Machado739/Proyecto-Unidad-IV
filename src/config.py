a1db3b713d9e0b1ddd1b67dc79ffa4c7ded40448
Alexis Machado <81934864+Machado739@users.noreply.github.com>
2023-12-14 03:11:03 -0800

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,11 +1,36 @@
+import mysql.connector
+
 class DevelopmentConfig:
     DEBUG = True
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "usuario1"
-    MYSQL_PASSWORD = "Machador0231."
-    MYSQL_DB = "store"
-    
+    MYSQL_HOST = "brzb0069vyov66ptxq0k-mysql.services.clever-cloud.com"
+    MYSQL_USER = "uwu7l1vlgnzkywe5"
+    MYSQL_PASSWORD = "u6FX0ndWZxqo73xlp8VI"
+    MYSQL_DB = "brzb0069vyov66ptxq0k"
+
 config = {
     "development": DevelopmentConfig
 }
 
+# Obtén la configuración específica para el entorno de desarrollo
+config_dev = config["development"]
+
+# Construye la cadena de conexión según la configuración
+conn_str = {
+    'host': config_dev.MYSQL_HOST,
+    'user': config_dev.MYSQL_USER,
+    'password': config_dev.MYSQL_PASSWORD,
+    'database': config_dev.MYSQL_DB,
+    'raise_on_warnings': True
+}
+
+# Intenta establecer la conexión
+try:
+    connection = mysql.connector.connect(**conn_str)
+    print("Conexión exitosa.")
+except mysql.connector.Error as err:
+    print(f"Error al conectar a la base de datos: {err}")
+finally:
+    # Cierra la conexión al finalizar
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Conexión cerrada.")
